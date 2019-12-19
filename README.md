e0927f2aa20c6721cb0810c58178802b87e96394
Leah Barnes <johnson.leahe@gmail.com>
2019-12-18 20:29:22 -0500

    add dynamic HTML, add write HTML file, add convert to PDF, update readme.

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # developer_profile_generator
+
+# Description
+
+A command line application that produces a dynamically generated PDF resume from a user provided GitHub profile.
+The generated resume a user's bio image and location from
+their GitHub profile as well as number of public repositories, followers, and following count. The headline
+of the generated PDF matches the color that the user picks
+from the initial prompts. 
+
+The purpose of this application is to provide stakeholders
+up-to-date information about members of a development team.
+Rather than navigating to each team member's GitHub profile, 
+this command-line application allows for quick and easy generation of profiles in PDF format. 
+
+# How It Works
+The Command-Line application functions as follows:
+1. User is prompted to enter a GitHub username and to pick a color from a pre-generated list. 
+2. Axios is used to retrieve user's GitHub data from the GitHub API
+3. An HTML file is generated dynamically and then saved to the computer.
+4. The HTML file is converted to a PDF file and saved to the computer. 
+
+# Installing
+1. Install dependencies by opening index.js in terminal and running command: npm install. 
+2. Run application with node.js by running command: node index.js
+3. Enter GitHub username when prompted.
+4. Use arrows and return button to select color from list. 
+5. Navigate to saved pdf file on computer and open.
+
+# Built With
+JavaScript
+Node.js
+
+# NPM
+HTML-PDF
+Inquirer 
+Axios
+File System
+
+# API
+GitHub API
+
--- hw8index.js
+++ hw8index.js
@@ -1,48 +1,95 @@
-// requires file system module
-const fs = require("fs");
-// requires axios module
-const axios = require("axios");
-// requires inquirer module
-const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require('axios');
+const inquirer = require('inquirer');
+// const util = require('util');
+const pdf = require('html-pdf');
+// const readFileAsync = util.promisify(fs.readFile);
 
-
-const markdown = require("markdown");
-
-// will prompt user/create object to pull from later
+// will prompt user for github username and to pick favorite color
 inquirer
     .prompt([{
+        type: "input",
         message: "Please enter GitHub username.",
         name: "username"
     },
     {
+        type: "list",
         message: "What is your favorite color?",
-        name: "color"
-    }])
+        name: "color",
+        choices: [
+            "Green",
+            "Blue",
+            "Pink",
+            "Red",
+        ]
+    }
+    ])
     // after prompt runs, then it will run function to access github profile based on username supplied
-    .then(function ({ username }) {
+    .then(function ({ username, color }) {
         // variable for query URL from github API
         const queryUrl = `https://api.github.com/users/${username}`;
 
-
-        console.log("got here");
         //   using axios to retrieve info from github API
         axios.get(queryUrl).then(function (res) {
             const githubData = res.data;
-            // , user.avatar_url,user.html_url, user.location, user.bio, user.public_repos, user.followers, user.following ;                       
-            // return githubData.data;
-            
-            
-            // console.log(githubData);
-            console.log(`name: ${githubData.name}`);
-            console.log(`location: ${githubData.location}`);
-            console.log(`avatar URL: ${githubData.avatar_url}`);
-            console.log(`Bio: ${githubData.bio}`);
-            console.log(`# of Repos: ${githubData.public_repos}`);
-            console.log(`# of Followers: ${githubData.followers}`);
-            console.log(`Following: ${githubData.following}`);
-            // use .map to get all of the the info into one array? 
+            // console.log(`name: ${githubData.name}`);
+            // console.log(`location: ${githubData.location}`);
+            // console.log(`avatar URL: ${githubData.avatar_url}`);
+            // console.log(`Bio: ${githubData.bio}`);
+            // console.log(`# of Repos: ${githubData.public_repos}`);
+            // console.log(`# of Followers: ${githubData.followers}`);
+            // console.log(`Following: ${githubData.following}`);
+            // console.log(`Link to Github: ${githubData.html_url}`);
+            // console.log(`Star: ${githubData.html_url}`);
+
+            // writes HTML File
+            fs.writeFile('first.html', `<!DOCTYPE html>
+            <html>
+            <head>
+              <meta charset="utf-8">
+              <title>Developer Profile</title>
+            </head>
+            <body>
+              <h1><span style="color:${color}"> ${githubData.name}</span></h1>
+            <hr>
+            <img src="${githubData.avatar_url} alt="profile-pic">
+            <br>
+            <p><h3>Bio:</h3> ${githubData.bio}
+            <br>
+            <h3>Location:</h3> ${githubData.location}
+            <br>
+            <h3>Link to GitHub:</h3><a href="${githubData.html_url}">Click</a>
+            <h3>Number of Public Repos:</h3> ${githubData.public_repos}
+            <br>
+            <h3>Number of Followers:</h3> ${githubData.followers}
+            <br>
+            <h3>Following:</h3> ${githubData.following}
+            </p>  
+            </body>
+            </html>`, function (err) {
+                if (err) throw err;
+                console.log('Saved HTML File');
+
+                // creates PDF file
+                const html = fs.readFileSync('./first.html', 'utf8');
+                const options = { format: 'Letter' };
+
+                pdf.create(html, options).toFile('.devprofile.pdf', function (err, res) {
+                    if (err) return console.log(err);
+                    console.log(res);
+                });
+            });
+
+
         });
     });
+
+
+
+
+
+
+
             // I want to write into a file, so i need to stringify. .join() means each item in array will be separated by the character I provide (new line in this case)
             // const githubDataStr = githubData.join("\n");
 
